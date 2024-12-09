506e83358dbe0753957c9d960e3421666d6cde20
Gopesh Sharma <gopesh.sharma@gmail.com>
2024-12-09 15:55:24 -0500

    completed app

---
+++ script.js
@@ -0,0 +1,21 @@
+let input = document.getElementById("inp");
+let buttons = document.querySelectorAll(".btn");
+let string = "";
+const arr = Array.from(buttons);
+arr.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
